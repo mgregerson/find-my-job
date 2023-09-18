16d6861c4d09082c658b383cb5297a4a5d612ad1
mgregerson <mgregerson4321@gmail.com>
2023-09-18 19:37:45 -0400

    Added JobColumn component that renders a column of jobs depending on status, ie. "APPLIED"

---
+++ src/app/components/core/JobColumn/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import AddJobButton from "../../button/AddJobButton";
+import JobCard from "../JobCard";
+
+export interface JobColumnProps {
+  title: string;
+  icon: string;
+  addJob: () => void;
+  jobs: any[];
+}
+
+const JobColumn = ({ title, icon, addJob, jobs }: JobColumnProps) => {
+  return (
+    <div
+      className="border-gray-200 border-2 text-center"
+      style={{ flex: "1 0 275px" }}
+    >
+      <div className="mt-10 font-bold">{title}</div>
+      <div className="text-gray-400">{jobs.length} Jobs</div>
+      <AddJobButton symbol={"+"} />
+      {jobs.map((job) => {
+        return (
+          <JobCard
+            job={job.job}
+            logo={"testlogo"}
+            company={job.company}
+            applyDate={job.applyDate}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default JobColumn;
